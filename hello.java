527c9d65ae1d4eacc7707322b07bc9380c1f44b7
indigo-mou5e <himajinn59@gmail.com>
2016-12-14 19:43:11 +0900

    hello.javaが動くようにした

--- hello.java
+++ hello.java
@@ -1,14 +1,14 @@
 public class hello{
-public static void main(){
+public static void main(String[] args){
 	for(int i=1;i<100;i++){
-		if(i%3){
-			if(i%5)
+		if(i%3==0){
+			if(i%5==0)
 				System.out.println("fizz buzz");
 			else
 				System.out.println("fizz");
 		}
 		else{
-			if(i%5)
+			if(i%5==0)
                                 System.out.println("buzz");
                         else
                                 System.out.println(i);
